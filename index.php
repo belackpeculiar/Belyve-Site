44aabb5161d8a64ef3c9c06c314b4cbf0bf54b8b
belackpeculiar <peculiarbelack@gmail.com>
2023-08-18 07:34:56 +0200

    created two files and modified a file

--- index.php
+++ index.php
@@ -4,8 +4,81 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belyve's Site</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1></h1>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid mt-3">
+            <a class="navbar-brand" href="#">
+                <img src="Screenshot (3).png" alt="LOGO" style="width: 65px;" class="rounded-pill">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+            <ul class="navbar-nav"> 
+            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#">About Us</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Mission</a></li>
+                    <li><a class="dropdown-item" href="#">Vision</a></li>
+                    <li><a class="dropdown-item" href="#">Team</a></li>
+                </ul>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#">Services<a>
+            <li class="nav-item"><a class="nav-link" href="#">Testimonials</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+            <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Online Booking</a>
+              <div class="sub-menu-1">
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Reservations</a></li>
+                    <li><a class="dropdown-item" href="#">Working Hours</a></li>
+                </ul>
+               </div>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#">Contacts</a></li>
+            <li class="nav-item"><a class="nav-link" href="logout.php" >Logout</a></li>
+          </ul>
+         </div>
+        </div>
+    </nav>
+    <div class="container-fluid bg-success p-3 text-white text-center">
+        <div class="row">
+            <div class="col-sm-6 pt-5">
+               <h1 class="text-uppercase">health without a side of body shame</h1>
+               <h3>nutrition for body maintenance</h3>
+               <h5 class="text-capitalize"> Feeling good about yourself is so much more important than striving for a certain body type — especially when that body type simply isn't obtainable.</h5>
+               <h5>Remember: Every body is different, so the weight loss plan that works for your friend may not work for you. Likewise, it's important to think about why you want to lose weight rather than simply doing so out of a feeling of obligation. A smaller number on the scale doesn't equate to health, after all, just as a smaller body is not necessarily a healthier one. If you're just getting started with your weight loss journey, then here are a few things to keep in mind:</h5>
+            </div>
+            <div class="col-sm-6" >
+                <img src="Screenshot (3).png" alt="" class="rounded img-fluid float-end">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-white p-3 text-white">
+        <div class="container">
+            <div class="container bg-danger p-3 text-white">
+                <div class="row">
+                    <div class="col-sm-4">
+                       <img src="Screenshot (3).png" alt="" class="rounded img-fluid">
+                    </div>
+                    <div class="col-sm-8">
+                       <h1>did you grow up in a family obsessed with weight and dieting?</h1>
+                       <h4>If generational diet trauma impacts your relationship to food and body image today, you're not alone. I'm Melissa, Registered Dietitian offering 1:1 nutrition therapy and body image coaching that takes into account your lived experience with lifelong diet cycles, disordered eating, eating disorders, and body image concerns.</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class=" mb-2 container bg-light p-3 text-white">
+                <a class="navbar-brand" href="#">
+                   <img src="Screenshot (3).png" alt="LOGO" style="width: 65px;" class="rounded-pill">
+                </a>
+                <div class="navbar">
+                   <h6 class="text-dark float-end">After the 1 to 1 program with Yvette, I have stopped dieting and no longer feel the need to. I have learned to trust my body and eat without guilt, a concept that baffled me during our first call.I have talked about my journey and healing my relationship with food with others, being vulnerable about something that would normally cause me to put up a wall. I’m learning my boundaries and how to cope with negative and positive emotions more than ever before without leaning restricting and binging.Yvette helped me change my life and learn to take care of my body after over a decade of learning to do the opposite - without dieting.</h6>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
